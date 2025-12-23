99ca1a8f460ac3dd472884aa7ee4448544c4e54a
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2025-12-24 00:57:18 +0530

    2 question practice done and topic short overview file done

---
+++ DAY-1/2-basic-temperature-converter.py
@@ -0,0 +1,13 @@
+#Q2 basic temperature converter Celsius and Fahrenheit
+
+#To know the conversion type
+num = int(input("Press 1 for (Celsius ↔ Fahrenheit) Press 2 for (Fahrenheit ↔ Celsius): "))
+
+if (num == 1):
+    c = int(input("Enter the Temperature: "))
+    print((9/5) * c + 32)
+elif (num == 2):
+    c = int(input("Enter the Temperature: "))
+    print((c - 32) * 5/9)
+else:
+    print("enter correct conversion type.")
