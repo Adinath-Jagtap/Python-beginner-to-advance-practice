1bd22727eebbca9d54e8d5ed17bb60d8d0d86b9f
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2025-12-24 01:05:40 +0530

    3 question practice done

--- DAY-1/2-basic-temperature-converter.py
+++ DAY-1/2-basic-temperature-converter.py
@@ -3,6 +3,7 @@
 #To know the conversion type
 num = int(input("Press 1 for (Celsius ↔ Fahrenheit) Press 2 for (Fahrenheit ↔ Celsius): "))
 
+# if and elif for proper conversions according to conversion type 
 if (num == 1):
     c = int(input("Enter the Temperature: "))
     print((9/5) * c + 32)
---
+++ DAY-1/3-even-odd.py
@@ -0,0 +1,10 @@
+#Q3 Even Odd Checker
+
+#input
+number = int(input("Enter the number: "))
+
+#check whether number is even or odd 
+if (number%2 == 0):
+    print(str(number) + " is even number")
+else:
+    print(str(number) + " is odd number")
