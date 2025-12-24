50db808aa7b751e2fe92a1619a6f15598321627f
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2025-12-25 00:24:27 +0530

    Q20 practice question solved

---
+++ DAY-2/10-tubles(list)-to-dictionary.py
@@ -0,0 +1,10 @@
+#Q10 Convert list of tuples to dictionary
+
+#sample data
+pairs = [("a", 1), ("b", 2), ("c", 3)]
+
+#list of tubles to dictionary
+my_dict = dict(pairs) 
+
+#printing the result 
+print(my_dict)
