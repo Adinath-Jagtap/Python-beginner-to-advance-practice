a66fd0624f1cccc562b69b997ec3b884920bcbf2
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2025-12-25 16:49:16 +0530

    DAY 3 Q7 Solved

---
+++ DAY-3/7-reverse-number.py
@@ -0,0 +1,10 @@
+#Q6 Reverse the input number
+
+#Input
+number = input("Enter Number: ")
+
+#reverse the number
+reversed_number = number[::-1]
+
+#printing the reversed number of input number
+print(int(reversed_number))
