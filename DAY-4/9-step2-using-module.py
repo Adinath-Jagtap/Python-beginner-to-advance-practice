7a7e8ea054998d336edd8de80c809cdd234cad0b
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2025-12-26 23:37:09 +0530

    Q9 DAY 4 Solved

---
+++ DAY-4/9-step2-using-module.py
@@ -0,0 +1,9 @@
+#Q9 Create module with 3 math functions, import and use
+#Step 2 : importing and using the module created
+
+import mymodule as m #importing the module created (mymodule.py)
+
+#example input for calling the functions
+m.square(4) 
+m.power(3,2)
+m.factorial(5)
---
+++ DAY-4/mymodule.py
@@ -0,0 +1,14 @@
+#Q9 Create module with 3 math functions, import and use
+#Step 1 : Creating a module
+
+import math #importing math module
+
+#Three math functions        
+def square(n):
+    print(math.sqrt(n))
+
+def power(a,b):
+    print(math.pow(a, b)) 
+
+def factorial(n):
+    print(math.factorial(n)) 
