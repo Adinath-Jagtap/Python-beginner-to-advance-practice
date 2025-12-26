a6be48b4176a2434db88fb215a9f6c64a149da11
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2025-12-26 21:25:47 +0530

    Q1 DAY 4 Solved

---
+++ DAY-4/1-check-leap-year.py
@@ -0,0 +1,12 @@
+#1 Function to check leap year
+
+#function for checking the input year is leap year or not
+def checker(num):
+    if (num % 4 == 0):
+        print(num,"is leap year")
+    else:
+        print(num,"is not leap year")
+
+#taking input of year
+a = int(input("Enter the year: "))
+checker(a) #calling the checker function
