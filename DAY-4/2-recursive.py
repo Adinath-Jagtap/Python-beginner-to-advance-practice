5f6144fe92661168b5a9130ced6e0c36e2f99957
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2025-12-26 21:50:14 +0530

    Q2 DAY 4 Solved

--- DAY-4/2-recursive.py
+++ DAY-4/2-recursive.py
@@ -1,10 +1,14 @@
 #Q2 Function to find factorial (recursive)
 
+#function for factorial
 def factorial(num):
     if (num == 0 or num == 1):
         return 1
     else:
-         return num*factorial(num-1)
+         return num*factorial(num-1) #recursion
 
+#taking input
 number = int(input("Enter the number: "))
+
+#printing result
 print(factorial(number),"is factorial of",number)
