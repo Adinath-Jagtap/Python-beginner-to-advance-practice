8da01ebb7ce1b491376ff81b14e64fb1a46e3f00
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2025-12-27 19:25:06 +0530

    Q3 Day 5 Solved

---
+++ DAY-5/3-Replace-spaces.py
@@ -0,0 +1,10 @@
+# Q3 Replace all spaces with underscores
+
+#input
+sentance = input("Enter the sentence: ")
+
+#replacing all spaces with underscores
+new_sentance = sentance.replace(" ","_")
+
+#printing the result
+print(new_sentance)
