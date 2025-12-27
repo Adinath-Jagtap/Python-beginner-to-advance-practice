191a06375e7cbeed29fe4e832a1885b8520e2cb3
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2025-12-27 20:07:49 +0530

    Q6 Day 5 Solved

---
+++ DAY-5/6-Join-list-strings.py
@@ -0,0 +1,8 @@
+# Q6 Join list of strings with separator
+
+#sample list
+words = ["Hello", "World", "Python","is","best"]
+
+# joining the list elements to make a proper sentence with " " as separtor
+sentence = " ".join(words)  
+print(sentence) #printing result
