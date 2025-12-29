9a1a738f62a1b23afc46b52ced005b8cd7aa40fd
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2025-12-29 20:30:30 +0530

    Q2 DAY 7 Solved

---
+++ DAY-7/Q2-Day-7.py
@@ -0,0 +1,10 @@
+# Q2 Create class with method that returns value
+
+#creating a class named calculator
+class Calculator:
+    def add(self,a,b): #adding method/function of the class
+        return a+b
+    
+#creating an object    
+addition  = Calculator()
+print(addition.add(4,5)) #printing method's output
