81774a92fb5be8e5bcc50b6920684f995254bc16
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2025-12-29 20:17:04 +0530

    Q1 DAY 7 Solved

---
+++ DAY-7/Q1-Day-7.py
@@ -0,0 +1,20 @@
+# Q7 Create class with attributes and print them
+
+# Created Class with attributes
+class Car:
+    brand = "Toyota"
+    wheels = 4
+    doors = 4
+
+#printing the attributes using "class".
+print(Car.brand)
+print(Car.wheels)
+print(Car.doors)
+
+#creating an object of class Car
+car1 = Car()
+
+#printing the attributes using the object creaed using Car class.
+print(car1.brand)
+print(car1.wheels)
+print(car1.doors)
