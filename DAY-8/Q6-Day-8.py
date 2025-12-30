8c85ed42259fce687b8c1b87daf32d77f92cf05c
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2025-12-30 12:47:30 +0530

    Q6 Day 8 Solved

---
+++ DAY-8/Q6-Day-8.py
@@ -0,0 +1,33 @@
+# Q6. Create a custom iterator class using __iter__ and __next__
+
+#creating Iterator class
+class Iterator:
+    def __init__(self, start, end): #constructor
+        self.current = start 
+        self.end = end
+
+    '''Iterable vs Iterator:
+        - Iterable: Object that can return an iterator (list, tuple, string)
+        - Iterator: Object that produces values one at a time
+    '''
+    def __iter__(self): #makes object a iterator
+        return self
+
+    def __next__(self): #returns next value if value exists
+        if self.current > self.end:
+            raise StopIteration #necessary to stop iteration when value doesn't exists
+        else:
+            value = self.current
+            self.current += 1
+            return value  #return value
+
+#creates counter object of Iterator class
+counter = Iterator(1, 5)
+for num in counter:
+    print(num)  #printing result
+
+# Manual iteration
+counter2 = Iterator(10, 12)
+print(next(counter2))
+print(next(counter2))  
+print(next(counter2))  
