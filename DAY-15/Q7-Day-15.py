a83fefe28f3510948410389aaaaa617eaa566e41
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2026-01-06 18:00:16 +0530

    Q67 Day 15 Solved

---
+++ DAY-15/Q7-Day-15.py
@@ -0,0 +1,14 @@
+# 7) Validate password (min 8 chars, 1 digit, 1 special char)
+import re
+
+passwords = ["password", "Passw0rd!", "short1!", "NoSpecial123", "Good@1234"]
+
+# Use lookahead assertions:
+# (?=.*\d)  -> at least one digit
+# (?=.*[^\w\s]) -> at least one special char (non-alphanumeric, not whitespace)
+# .{8,} -> at least 8 characters total
+pwd_pattern = r"^(?=.*\d)(?=.*[^\w\s]).{8,}$"
+
+for pwd in passwords:
+    ok = bool(re.fullmatch(pwd_pattern, pwd))
+    print(f"{pwd:12} -> valid? {ok}")
