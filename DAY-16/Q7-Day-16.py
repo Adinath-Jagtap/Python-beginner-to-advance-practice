58a45f67b2cf7557034f071623d2eb5aad5d4356
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2026-01-07 22:46:44 +0530

    Q7 Day 16 Solved

--- DAY-16/Q7-Day-16.py
+++ DAY-16/Q7-Day-16.py
@@ -2,10 +2,10 @@
 
 import pandas as pd
 
-df = pd.read_csv("sales.csv")
+df = pd.read_csv("DAY-16/sample-data-files/sales.csv")
 # filter: keep rows with units >= 7
 filtered = df[df["units"] >= 7]
 # group by 'store' and sum units & revenue
 grouped = filtered.groupby("store").agg({"units":"sum", "revenue":"sum"}).reset_index()
 print(grouped)
-grouped.to_csv("grouped_sales.csv", index=False)
+grouped.to_csv("Q7_grouped_sales.csv", index=False)
