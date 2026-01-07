854def5ce2251b7ed0ab4b5eb9d76b8c3ed7f73d
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2026-01-07 23:00:02 +0530

    Q12 Day 16 Solved

---
+++ DAY-16/Q12-Day-16.py
@@ -0,0 +1,5 @@
+# 12. Use regex to extract all dates in the format DD-MM-YYYY from a text
+
+sentence = "Hello world from Python"
+reversed_words = " ".join(word[::-1] for word in sentence.split())
+print(reversed_words)  # "olleH dlrow morf nohtyP"
