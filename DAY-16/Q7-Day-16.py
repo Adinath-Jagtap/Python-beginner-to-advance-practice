763caf63ec5afea19cd7768032924ae5bb8f30fc
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2026-01-07 22:44:09 +0530

    Q7 Day 16 Solved

---
+++ DAY-16/Q7-Day-16.py
@@ -0,0 +1,11 @@
+#7. Load a CSV into a Pandas DataFrame, filter rows, group by a column, and calculate the sum
+
+import pandas as pd
+
+df = pd.read_csv("sales.csv")
+# filter: keep rows with units >= 7
+filtered = df[df["units"] >= 7]
+# group by 'store' and sum units & revenue
+grouped = filtered.groupby("store").agg({"units":"sum", "revenue":"sum"}).reset_index()
+print(grouped)
+grouped.to_csv("grouped_sales.csv", index=False)
