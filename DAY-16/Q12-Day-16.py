59927683e71f1b722c77dc293e2287559ceea6f5
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2026-01-07 23:00:47 +0530

    Q12 Day 16 Solved

--- DAY-16/Q12-Day-16.py
+++ DAY-16/Q12-Day-16.py
@@ -1,5 +1,7 @@
 # 12. Use regex to extract all dates in the format DD-MM-YYYY from a text
 
-sentence = "Hello world from Python"
-reversed_words = " ".join(word[::-1] for word in sentence.split())
-print(reversed_words)  # "olleH dlrow morf nohtyP"
+import re
+text = "Events: 01-01-2020, invalid 32-13-2020, 15-08-1947 and 05-06-2021."
+pattern = r"\b\d{2}-\d{2}-\d{4}\b"
+dates = re.findall(pattern, text)
+print(dates)  # ['01-01-2020', '15-08-1947', '05-06-2021']
