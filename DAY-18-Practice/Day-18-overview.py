021371f1018a77515b09af520ef7756553348a6c
Adinath-Jagtap <adinathjagtap9702@gmail.com>
2026-01-20 23:15:01 +0530

    DAY 18 done

---
+++ DAY-18-Practice/Day-18-overview.py
@@ -0,0 +1,10 @@
+# =============================================================================
+# PYTHON PROBLEMS (2 - EASY)
+# =============================================================================
+'''
+1. Reverse a string
+   - Given a string, return the reversed version of it.
+
+2. Check if a number is prime
+   - Given an integer, determine whether it is a prime number.
+'''
